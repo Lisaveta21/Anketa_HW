8468fc057d3be5088e952ef496ecb3ba6bd6716a
Lisa <Liza@desktop>
2023-06-12 22:43:07 +0300

    Add project files.

---
+++ Anketa_HW/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Anketa_HW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Surname = new System.Windows.Forms.TextBox();
+            this.SecondName = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(529, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Список";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Отчество (при наличии)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Дата рождения";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 387);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(306, 183);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(147, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Для любителей астрологии";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(246, 37);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(207, 20);
+            this.Name.TabIndex = 7;
+            // 
+            // Surname
+            // 
+            this.Surname.Location = new System.Drawing.Point(246, 78);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(207, 20);
+            this.Surname.TabIndex = 8;
+            // 
+            // SecondName
+            // 
+            this.SecondName.Location = new System.Drawing.Point(246, 130);
+            this.SecondName.Name = "SecondName";
+            this.SecondName.Size = new System.Drawing.Size(207, 20);
+            this.SecondName.TabIndex = 9;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(120, 189);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 10;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(246, 384);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(207, 20);
+            this.Email.TabIndex = 11;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(521, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 329);
+            this.listBox1.TabIndex = 12;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(38, 432);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(121, 19);
+            this.AddBtn.TabIndex = 13;
+            this.AddBtn.Text = "Добавить";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 19);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Сохранить в файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 28);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 28);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(459, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(31, 28);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(459, 384);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(31, 28);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.SecondName);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+          
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.TextBox Surname;
+        private System.Windows.Forms.TextBox SecondName;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Anketa_HW/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anketa_HW
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Anketes> ankets = new BindingList<Anketes>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.DataSource = ankets;
+            listBox1.DisplayMember = "Name";
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
+        }
+
+        public class Anketes
+        {
+        
+                public string name { get; set;}
+            public string surname { get; set;}
+            public string secondname { get; set; }
+            public string email { get; set;}
+
+        
+        }
+
+  private void AddBtn_Click(object sender, EventArgs e)
+        {
+            string email = Email.Text;
+            Anketes anketes = new Anketes();
+            anketes.name = Name.Text;
+            anketes.email = Email.Text;
+            anketes.surname = Surname.Text;
+            anketes.secondname = SecondName.Text;
+            ankets.Add(anketes);        
+            listBox1.SelectedIndex = -1;
+
+        }
+
+        //метод позволяющий в поля вводить только буквы и backspace
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            listBox1.Text = String.Format(" ", e.Start.ToLongDateString()); 
+
+        }
+
+        private void VisitLink()
+        { 
+        
+                linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start(" https://geocult.ru/natalnaya-karta-onlayn-raschet ");
+        
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+           
+                VisitLink();
+
+            
+           
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog1.FileName;
+
+
+                using (StreamWriter sw = new StreamWriter(fileName))
+                {
+                    foreach (var item in ankets)
+                    {
+                        sw.WriteLine(item.name);
+                        sw.WriteLine(item.secondname);
+                        sw.WriteLine(item.email);
+                        sw.WriteLine(item.surname);
+
+                    }
+                }
+            }
+
+        }
+
+        
+    }
+}
